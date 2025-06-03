4bcad532463ca99da194edb65f82f1e3d0444560
matt enbar <62265102+mattenbar@users.noreply.github.com>
2025-06-03 13:00:34 -0700

    cloudname

--- upload/lesson1/example6.js
+++ upload/lesson1/example6.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.uploader.upload('assets/images/kitten.jpg', {
-        folder: 'cute_animals',
+        folder: 'cute_animals2',
         public_id: 'kitten'
     })
     .then(uploadResult => console.log(uploadResult))
